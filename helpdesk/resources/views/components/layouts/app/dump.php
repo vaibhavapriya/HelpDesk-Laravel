2e7bcd3f4ee2dabdf681b396217ada20976d71d4
vaibhavapriyandv@gmail.com <vaibhavapriyandv@gmail.com>
2025-05-19 12:13:12 +0530

    static pages

--- helpdesk/resources/views/components/layouts/app/dump.php
+++ helpdesk/resources/views/components/layouts/app/dump.php
@@ -1,66 +1,6 @@
-
-  <!-- Navbar -->
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-      <!-- Brand -->
-      <a class="navbar-brand me-5" href="home">HELPDESK</a>
-
-      <!-- Toggle button for mobile (optional if you want it collapsible) -->
-      <!-- Uncomment below if you want it responsive -->
-      
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-     
-
-      <!-- Menu -->
-      <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-      <ul class="navbar-nav">
-        <?php 
-          if (!isset($_SESSION['jwt_token']) || empty($_SESSION['jwt_token'])) { ?>
-            <li class="nav-item">
-              <a class="nav-link" href="newTicket">SUBMIT TICKET</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="knowledgeBase">KNOWLEDGEBASE</a>
-            </li>
-            <li class="nav-item">
-              <a class="btn btn-primary" href="login">LOGIN</a>
-            </li>
-        <?php } else { ?>
-            <li class="nav-item">
-              <a class="nav-link" href="newTicket">SUBMIT TICKET</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="knowledgeBase">KNOWLEDGEBASE</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="myTickets">MY TICKET</a>
-            </li>
-            <!-- User Dropdown -->
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                <i class="far fa-user"></i>
-              </a>
-              <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="userDropdown">
-                <?php if (isset($_SESSION['role']) && $_SESSION['role'] === 'admin'): ?>
-                  <li><a class="dropdown-item" href="adminhome"><i class="fas fa-user-shield me-2"></i> Admin Portal</a></li>
-                <?php endif; ?>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="dropdown-item" href="profile"><i class="fas fa-sign-out-alt me-2"></i> My Profile</a></li>
-                <li><a class="dropdown-item" href="logout"><i class="fas fa-sign-out-alt me-2"></i> Logout</a></li>
-              </ul>
-            </li>
-        <?php } ?>
-      </ul> 
-      </div>
-      <!-- @if (Route::has('login'))
-            <div class="h-14.5 hidden lg:block"></div>
-        @endif -->
-
-    </div>
-  </nav>
+<!-- css files -->
+@source '../../vendor/livewire/flux-pro/stubs/**/*.blade.php'; 
+@source '../../vendor/livewire/flux/stubs/**/*.blade.php';
 
 <!-- registeration -->
 https://epicbootstrap.com/snippets/registration
--- helpdesk/resources/views/welcome.blade.php
+++ helpdesk/resources/views/welcome.blade.php
@@ -1,2 +1,56 @@
 @extends('components.layouts.app.client')
-@section('content','Home Page')
+@section('content')
+<main class="d-flex align-items-center justify-content-center ">
+    <div class="container">
+        <div class="row g-4 mb-5">
+
+            <div class='col-12 col-sm-6 col-md-4 col-lg-3'>
+            <a href="register" class="text-decoration-none text-dark">
+                <div class="card text-center shadow-sm bg-light">
+                <div class="card-body">
+                    <i class="fa-regular fa-pen-to-square fa-2x mb-2"></i>
+                    <h5 class="card-title">Register</h5>
+                </div>
+                </div>
+            </a>
+            </div>
+
+        <!-- Card 2 -->
+        <div class='col-12 col-sm-6 col-md-4 col-lg-3'>
+            <a href="newTicket" class="text-decoration-none text-dark">
+                <div class="card text-center shadow-sm bg-light">
+                    <div class="card-body ">
+                        <i class="fa-solid fa-rectangle-list fa-2x mb-2"></i>
+                        <h5 class="card-title">Submit Ticket</h5>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <!-- Card 3 -->
+        <div class='col-12 col-sm-6 col-md-4 col-lg-3'>
+            <a href="myTickets" class="text-decoration-none text-dark">
+                <div class="card text-center shadow-sm bg-light">
+                    <div class="card-body">
+                        <i class="fa-regular fa-newspaper fa-2x mb-2"></i>
+                        <h5 class="card-title">My Ticket</h5>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <!-- Card 4 -->
+        <div class='col-12 col-sm-6 col-md-4 col-lg-3'>
+            <a href="knowledgeBase" class="text-decoration-none text-dark ">
+                <div class="card text-center shadow-sm bg-light">
+                    <div class="card-body">
+                        <i class="fa-solid fa-lightbulb  fa-2x mb-2"></i>
+                        <h5 class="card-title">Knowledgebase</h5>
+                    </div>
+                </div>
+            </a>
+        </div>
+        </div>
+    </div>
+</main>
+@endsection
